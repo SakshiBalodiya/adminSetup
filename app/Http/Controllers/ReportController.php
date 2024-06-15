e4512f5055cdcb52557776b411330ef8c57690f9
SakshiBalodiya <sakshi@majestictechnologies.net>
2024-06-15 18:40:58 +0530

    changes

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CalenderController extends Controller
+class ReportController extends Controller
 {
+    //
     public function admin_index()
     {
-        return view('admin.calender.calender');
+        return view('admin.report.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CalenderController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\StaffController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -37,5 +38,6 @@ Route::get('attendance', [AttendanceController::class, 'admin_index']);
 
 Route::get('calender', [CalenderController::class, 'admin_index']);
 
+Route::get('report', [ReportController::class, 'admin_index']);
 
 Route::post('/logout', [LoginController::class, 'destroy'])->middleware('auth')->name('logout');
